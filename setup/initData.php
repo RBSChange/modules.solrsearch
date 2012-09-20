f9ee156ca1684bd5e358f63466d0e5dc33e4841b
Gaël PORT <gael.port@rbs.fr>
2012-09-20 09:14:40 +0200

    [DELETE] Delete old samples.

--- setup/initData.php
+++ setup/initData.php
@@ -3,14 +3,6 @@ class solrsearch_Setup extends object_InitDataSetup
 {
 	public function install()
 	{
-		try
-		{
-			$scriptReader = import_ScriptReader::getInstance();
-			$scriptReader->executeModuleScript('solrsearch', 'init.xml');
-		}
-		catch (Exception $e)
-		{
-			Framework::exception($e);
-		}
+		$this->executeModuleScript('init.xml');
 	}
 }
